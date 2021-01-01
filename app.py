5f49b747b5261fdda25dbbecffdd1a9053b960af
Henry <oneiromancy.ai@gmail.com>
2021-01-01 20:41:03 +0000

    designing database tables and running migrations

--- app.py
+++ app.py
@@ -28,6 +28,52 @@ csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+class Venue(db.Model):
+    __tablename__ = "Venue"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
+
+
+class Artist(db.Model):
+    __tablename__ = "Artist"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
+
+
+class Show(db.Model):
+    __tablename__ = "Show"
+
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"), nullable=False)
+    start_time = db.Column(db.DateTime())
+    venue = db.relationship("Venue", backref=db.backref("shows", cascade="all, delete"))
+    artist = db.relationship(
+        "Artist", backref=db.backref("shows", cascade="all, delete")
+    )
+
+
 
 @app.route('/')
 def index():
